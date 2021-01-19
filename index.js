16638a3b8419038aa1c62410f900b898aae8a0a4
turpp <white.tripp@icloud.com>
2021-01-19 12:30:38 -0600

    commit tree 809291369c43a66c1d6b84ee8ac02155a38b4b31

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Your code here
+function mapToNegativize(array){
+    let a =[]
+    for(let i=0; i < array.length; i++){
+        a.push(array[i]*-1)
+    }
+    return a
+}
+
+function mapToNoChange(array){
+    return array
+}
+
+function mapToDouble(array){
+    let a =[]
+    for(let i = 0; i<array.length; i++){
+        a.push(array[i]*2)
+    }
+    return a
+}
+
+function mapToSquare(array){
+    let a =[]
+    for(let i = 0; i < array.length; i ++){
+        a.push(array[i]*array[i])
+    }
+    return a
+}
+
+function reduceToTotal(array, start=0){
+    let base = start
+    for(let i = 0; i < array.length; i ++){
+        base += array[i]
+    }
+    return base
+}
+
+function reduceToAllTrue(array){
+    let a = []
+    for(let i = 0; i < array.length; i ++){
+        if (array[i]){
+            a.push(array[i])
+        }
+    }
+    if (a.length == array.length){
+        return true
+    }else{
+        return false
+    }
+}
+
+function reduceToAnyTrue(array){
+    let a = []
+    for(let i = 0; i < array.length; i ++){
+        if (array[i]){
+            a.push(array[i])
+        }
+    }
+    if(a.length > 0){
+        return true
+    }else{
+        return false
+    }
+}
